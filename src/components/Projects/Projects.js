6e19747539232ca8ea57617e7af869185832dc9c
Al-Monaem Khan <monaem.khan@sslwireless.com>
2024-04-21 16:53:56 +0600

    divider added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,54 +1,54 @@
-import Project from './Project';
-import style from './style';
-import './style.css';
+import Project from "./Project";
+import style from "./style";
+import "./style.css";
 
-import metro from '../../assets/metro/metro.png';
-import metro_login from '../../assets/metro/login.png';
-import metro_admin from '../../assets/metro/admin.png';
-import metro_active from '../../assets/metro/active.png';
-import metro_dashboard from '../../assets/metro/dashboard.png';
-import metro_recharge from '../../assets/metro/recharge.png';
-import metro_users from '../../assets/metro/users.png';
+import metro from "../../assets/metro/metro.png";
+import metro_login from "../../assets/metro/login.png";
+import metro_admin from "../../assets/metro/admin.png";
+import metro_active from "../../assets/metro/active.png";
+import metro_dashboard from "../../assets/metro/dashboard.png";
+import metro_recharge from "../../assets/metro/recharge.png";
+import metro_users from "../../assets/metro/users.png";
 
-import hms from '../../assets/hms/hms.png';
-import hms_login from '../../assets/hms/login.png';
-import hms_edit from '../../assets/hms/edit.png';
-import hms_tracks from '../../assets/hms/tracks.png';
-import hms_users from '../../assets/hms/users.png';
-import hms_register from '../../assets/hms/register.png';
+import hms from "../../assets/hms/hms.png";
+import hms_login from "../../assets/hms/login.png";
+import hms_edit from "../../assets/hms/edit.png";
+import hms_tracks from "../../assets/hms/tracks.png";
+import hms_users from "../../assets/hms/users.png";
+import hms_register from "../../assets/hms/register.png";
 
-import exam from '../../assets/exam/exam.png';
-import exam_create from '../../assets/exam/create.png';
-import exam_schedule from '../../assets/exam/schedule.png';
+import exam from "../../assets/exam/exam.png";
+import exam_create from "../../assets/exam/create.png";
+import exam_schedule from "../../assets/exam/schedule.png";
 
-import mfr from '../../assets/mfr/mfr.png';
-import mfr_login from '../../assets/mfr/login.png';
-import mfr_alert_light from '../../assets/mfr/alert-white.png';
-import mfr_alert_dark from '../../assets/mfr/alert-dark.png';
-import mfr_attendance_dark from '../../assets/mfr/attendance-dark.png';
-import mfr_attendance_light from '../../assets/mfr/attendance-light.png';
-import mfr_clients_dark from '../../assets/mfr/clients-dark.png';
-import mfr_clients_light from '../../assets/mfr/clients-light.png';
-import mfr_reconciliations_dark from '../../assets/mfr/reconciliations-dark.png';
-import mfr_reconciliations_light from '../../assets/mfr/reconciliations-light.png';
-import mfr_settings_dark from '../../assets/mfr/settings-dark.png';
-import mfr_settings_light from '../../assets/mfr/settings-light.png';
+import mfr from "../../assets/mfr/mfr.png";
+import mfr_login from "../../assets/mfr/login.png";
+import mfr_alert_light from "../../assets/mfr/alert-white.png";
+import mfr_alert_dark from "../../assets/mfr/alert-dark.png";
+import mfr_attendance_dark from "../../assets/mfr/attendance-dark.png";
+import mfr_attendance_light from "../../assets/mfr/attendance-light.png";
+import mfr_clients_dark from "../../assets/mfr/clients-dark.png";
+import mfr_clients_light from "../../assets/mfr/clients-light.png";
+import mfr_reconciliations_dark from "../../assets/mfr/reconciliations-dark.png";
+import mfr_reconciliations_light from "../../assets/mfr/reconciliations-light.png";
+import mfr_settings_dark from "../../assets/mfr/settings-dark.png";
+import mfr_settings_light from "../../assets/mfr/settings-light.png";
 
-import othership from '../../assets/othership/othership.png';
-import othership_planner from '../../assets/othership/planner.png';
-import othership_floor_plan from '../../assets/othership/floor-plan.png';
-import othership_groups from '../../assets/othership/groups.png';
-import othership_events from '../../assets/othership/events.png';
-import othership_settings from '../../assets/othership/settings.png';
-import othership_analytics from '../../assets/othership/analytics.png';
+import othership from "../../assets/othership/othership.png";
+import othership_planner from "../../assets/othership/planner.png";
+import othership_floor_plan from "../../assets/othership/floor-plan.png";
+import othership_groups from "../../assets/othership/groups.png";
+import othership_events from "../../assets/othership/events.png";
+import othership_settings from "../../assets/othership/settings.png";
+import othership_analytics from "../../assets/othership/analytics.png";
 
-import { useEffect, useState } from 'react';
-import ReactModal from 'react-modal';
-import { Divider } from 'antd';
-import { FaConnectdevelop } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import ReactModal from "react-modal";
+import { Divider } from "antd";
+import { FaConnectdevelop } from "react-icons/fa";
 
 const metroDescription =
-  'The metro system was built so that the daily users could get the exact information of what they need. One of the main feature of the system is live tracking of the train. There is also a ticket system integrated with mobile banking. A new QR ticket verification process is also added to the system to prevent fraud. Users can purchase ticket and store that ticket for as long as they want. They can refund any valid ticket at any time. Also a exclusive wallet system is introduced, by which users can purchase tickets using in-app currencies.';
+  "The metro system was built so that the daily users could get the exact information of what they need. One of the main feature of the system is live tracking of the train. There is also a ticket system integrated with mobile banking. A new QR ticket verification process is also added to the system to prevent fraud. Users can purchase ticket and store that ticket for as long as they want. They can refund any valid ticket at any time. Also a exclusive wallet system is introduced, by which users can purchase tickets using in-app currencies.";
 const metroImages = [
   metro_login,
   metro_admin,
@@ -58,26 +58,26 @@ const metroImages = [
   metro_users,
 ];
 const metroSkills = [
-  'ReactJS',
-  'NodeJS',
-  'Laravel',
-  'Asp.NET',
-  'Tailwind',
-  'Socket.Io',
+  "ReactJS",
+  "NodeJS",
+  "Laravel",
+  "Asp.NET",
+  "Tailwind",
+  "Socket.Io",
 ];
 
 const hmsDescription =
-  'This was my first web-app project. This is a simple hospital management system which manages users, patients, doctors and nurses. The motive behind this project was to completely digitalize the current management systems in the hospitals.';
+  "This was my first web-app project. This is a simple hospital management system which manages users, patients, doctors and nurses. The motive behind this project was to completely digitalize the current management systems in the hospitals.";
 const hmsImages = [hms_login, hms_register, hms_edit, hms_tracks, hms_users];
-const hmsSkills = ['Php', 'JQuery', 'HTML', 'CSS3'];
+const hmsSkills = ["Php", "JQuery", "HTML", "CSS3"];
 
 const examDescription =
   "I'm still working on this project. The system will allow teachers to schedule an exam for their students. They can manage sections, and assign exams to sections. For registered students, their will be exclusive material as well as scheduled exam, but for an unregistered student, there will be practice materials.";
 const examImages = [exam_create, exam_schedule];
-const examSkills = ['MERN', 'SocketI/O', 'Tailwind', 'Redux', 'JWT'];
+const examSkills = ["MERN", "SocketI/O", "Tailwind", "Redux", "JWT"];
 
 const mfrDescription =
-  'This is an admin panel made by me for my current company for a face recognition system. The system is customizable for when to generate alert, count attendance through face recognition. To be detected as a recognized category, user must onboard through a link given by a organization registered into our MFR system. Upon registering, the admin can then set specific rules for that onboarded person or create global customization for any onboarded person under that organization.';
+  "This is an admin panel made by me for my current company for a face recognition system. The system is customizable for when to generate alert, count attendance through face recognition. To be detected as a recognized category, user must onboard through a link given by a organization registered into our MFR system. Upon registering, the admin can then set specific rules for that onboarded person or create global customization for any onboarded person under that organization.";
 const mfrImagesLight = [
   mfr_login,
   mfr_alert_light,
@@ -94,7 +94,7 @@ const mfrImagesDark = [
   mfr_reconciliations_dark,
   mfr_settings_dark,
 ];
-const mfrSkills = ['ReactJS', 'Ant Design', 'Tailwind', 'Redux'];
+const mfrSkills = ["ReactJS", "Ant Design", "Tailwind", "Redux"];
 
 const othershipDescription = `Otherhship Workplace Scheduler is a hybrid workspace management system that provides a platform for employees to book their workspace in the office. 
 The system also provides a dashboard for the admin to manage the workspace and the employees. 
@@ -111,7 +111,7 @@ const othershipImages = [
   othership_settings,
   othership_analytics,
 ];
-const othershipSkills = ['NodeJS', 'NestJS', 'Queue', 'Scheduler', 'Webhooks'];
+const othershipSkills = ["NodeJS", "NestJS", "Queue", "Scheduler", "Webhooks"];
 
 const Projects = ({ refs }) => {
   const [openModal, setOpenModal] = useState(false);
@@ -145,15 +145,15 @@ const Projects = ({ refs }) => {
 
   // create an event listener
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   const onClick = (e) => {
-    const img = e.target.getAttribute('src');
+    const img = e.target.getAttribute("src");
     setOpenModal(true);
     setModalImage(img);
   };
@@ -177,18 +177,21 @@ const Projects = ({ refs }) => {
         skills={othershipSkills}
         image={othership}
         images={othershipImages}
-        align={'left'}
+        align={"left"}
         description={othershipDescription}
         moreDescription={moreDescription}
         hasLiveURL={true}
       />
+      <Divider>
+        <FaConnectdevelop className="h-5 w-5 text-gray-400" />
+      </Divider>
       <Project
         onClick={onClick}
-        title={'MFR'}
+        title={"MFR"}
         skills={mfrSkills}
         image={mfr}
         images={mfrImages}
-        align={'right'}
+        align={"right"}
         description={mfrDescription}
         hasDarkMode={true}
         switchTheme={switchTheme}
@@ -198,11 +201,11 @@ const Projects = ({ refs }) => {
       </Divider>
       <Project
         onClick={onClick}
-        title={'Exam Platform'}
+        title={"Exam Platform"}
         skills={examSkills}
         image={exam}
         images={examImages}
-        align={'left'}
+        align={"left"}
         description={examDescription}
       />
       <Divider>
@@ -211,11 +214,11 @@ const Projects = ({ refs }) => {
       <Project
         isMobile={isMobile}
         onClick={onClick}
-        title={'Metro e-Service'}
+        title={"Metro e-Service"}
         skills={metroSkills}
         image={metro}
         images={metroImages}
-        align={'right'}
+        align={"right"}
         description={metroDescription}
       />
       {/* <Divider>
@@ -236,11 +239,11 @@ const Projects = ({ refs }) => {
       <Project
         isMobile={isMobile}
         onClick={onClick}
-        title={'Hospital Management System'}
+        title={"Hospital Management System"}
         skills={hmsSkills}
         image={hms}
         images={hmsImages}
-        align={'left'}
+        align={"left"}
         description={hmsDescription}
       />
 
